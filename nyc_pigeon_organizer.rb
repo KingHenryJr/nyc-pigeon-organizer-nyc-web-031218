64c2e5f8051f3e39c5e49d4aadc3cd3618d25a1b
Henry Zhang <henryyihengzhang@gmail.com>
2018-03-03 15:49:03 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  new = {}
+  new_list = {}
   #sort or sort by?
   data.each do |info, datahash|
     datahash.each do |stuff, names|
       names.each do |pigeon|
-        if !new[pigeon].include?(pigeon) 
-          new[pigeon] = {}
+        if !new_list[pigeon].include?(pigeon) 
+          new_list[pigeon] = {}
   binding.pry
         end
       end 
